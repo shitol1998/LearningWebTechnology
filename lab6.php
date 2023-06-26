d139f33a01af9b9f99f1d843a70669d895033627
shitol1998 <110344268+shitol1998@users.noreply.github.com>
2023-06-26 15:03:09 +0600

    Add files via upload

---
+++ lab6.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Product details</title>
+</head>
+<body>
+    <fieldset>
+    <legend><b>Add Product</b></legend>
+    <label for="Name">Name</label>
+<input type="text"  name="name" value="">
+<br>
+<br>
+<label for="Buying Price">Buying Price</label>
+<input type="text"  name="name" value="">
+<br>
+<br>
+<label for="Selling price">Selling Price</label>
+<input type="text"  name="name" value="">
+<hr>
+<input type="checkbox" name="Display" value="Display">
+<label for="Display">Display</label>
+    
+    <br>
+    <br>
+<input type="submit" name="submit" value="submit">
+    </fieldset>
+</body>
+</html>
